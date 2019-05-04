9dd09ccfc942c784ad91691d335ee4e966546c85
Uysal4319 <ertuysal.55@gmail.com>
2019-05-04 16:25:27 +0300

    Initial Commit

--- src/BetweenTwoSets.java
+++ src/BetweenTwoSets.java
@@ -10,12 +10,79 @@ public class BetweenTwoSets {
      * Complete the getTotalX function below.
      */
     static int getTotalX(int[] a, int[] b) {
-   return 1; }
+        int[] divided = new int[100];
+        int count = 0;
+        int biggerB = b[0];
+        int biggerA = a[0];
+        for(int i = 0;i<b.length;i++){
+            for (int j =0; j<b.length-1;j++){
+              if(b[j+1] < b[j]){
+                  biggerB = b[j];
+                  b[j]= b[j+1];
+                  b[j+1]=biggerB;
+              }
+            }
+        }
+
+        for(int i = 0;i<a.length;i++){
+            for (int j =0; j<a.length-1;j++){
+                if(a[j+1] < a[j]){
+                    biggerA = a[j];
+                    a[j]= a[j+1];
+                    a[j+1]=biggerA;
+                }
+            }
+        }
+
+        for (int i = a[a.length - 1]; i <= b[0]; i++) {
+            if(a.length>1){
+                boolean divideFlag = true;
+                for (int j = 0; j < a.length ; j++) {
+                    if (i % a[j] != 0) {
+                     divideFlag = false;
+                    }
+                }
+                if (divideFlag){
+                    divided[count] = i;
+                    count++;
+                }
+            }else if (a.length == 1){
+                boolean divideFlag = true;
+                for (int j = 0; j < a.length ; j++) {
+                    if (i % a[j] != 0) {
+                       divideFlag =false;
+                    }
+                }
+                if (divideFlag){
+                    divided[count] = i;
+                    count++;
+                }
+            }
+
+        }
+
+
+        int result = 0;
+        while (count > 0) {
+            boolean flag = true;
+            for (int i = 0; i < b.length; i++) {
+                if (b[i] % divided[count - 1] != 0) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                result++;
+            }
+            count--;
+        }
+
+        return result;
+    }
 
     private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        //example input -4 3 -9 0 4 1
+
         int m = scan.nextInt();
         int n = scan.nextInt();
 
@@ -27,9 +94,9 @@ public class BetweenTwoSets {
 
         int[] b = new int[n];
 
-        for (int i = 0; i <n; i++) {
+        for (int i = 0; i < n; i++) {
             b[i] = scan.nextInt();
         }
-        System.out.println(getTotalX(a,b));
+        System.out.println(getTotalX(a, b));
     }
 }
