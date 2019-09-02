1efb0923dc1dc7983894212a7adec3e1494770ff
ertugruluy <ertugrul.uysal@argela.com.tr>
2019-09-02 10:20:48 +0300

    drawingBook algoritms completed

---
+++ src/SequenceEquation.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SequenceEquation {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p) {
+
+        return p;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+    int arraySize = scanner.nextInt();
+
+    int[] array = new int[arraySize];
+
+    for (int i= 0 ;i<arraySize;i++){
+        array[i] = scanner.nextInt();
+    }
+
+    permutationEquation(array);
+
+    }
+}
