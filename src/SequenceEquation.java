a91cd57c68e9c36263e51febaa0d78f9dfcaf569
ertugruluy <ertugrul.uysal@argela.com.tr>
2019-09-02 11:45:14 +0300

    SequenceEquation algoritms completed

--- src/SequenceEquation.java
+++ src/SequenceEquation.java
@@ -1,31 +1,51 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class SequenceEquation {
 
     // Complete the permutationEquation function below.
     static int[] permutationEquation(int[] p) {
 
-        return p;
+        int[] result = new int[p.length];
+        List<Integer> temp = new ArrayList<>();
+
+
+        for (int i = 0; i < p.length; i++) {
+            temp.add(p[p[i] - 1]);
+        }
+
+        for (int i = 0; i < p.length; i++) {
+            result[i] = temp.indexOf(i + 1) + 1;
+        }
+
+        return result;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-    int arraySize = scanner.nextInt();
+        int arraySize = scanner.nextInt();
 
-    int[] array = new int[arraySize];
+        int[] array = new int[arraySize];
 
-    for (int i= 0 ;i<arraySize;i++){
-        array[i] = scanner.nextInt();
-    }
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = scanner.nextInt();
+        }
 
-    permutationEquation(array);
+        System.out.println(permutationEquation(array));
 
     }
 }
+
+
+/*
+Complete the permutationEquation function in the editor below. It should return an array of integers that represent the values of y .
+
+permutationEquation has the following parameter(s):
+
+p: an array of integers
+
+
+*/
